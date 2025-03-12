c012de4e4a792319fcdaa8179c53bdd76d7d6c68
rohit <rohiitrb@gmail.com>
2025-03-12 09:15:38 +0530

    updated path

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,16 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Coding_mountain_react_dev_home_page from "./coding-mountain-react-dev/pages/homePage";
 import ProductDetail from "./coding-mountain-react-dev/components/ProductDetail";
 import ProductList from "./coding-mountain-react-dev/components/ProductList";
-import HomePage from "./rohitsx/Rohit";
+//import HomePage from "./rohitsx/Rohit";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/coding-mountain-react-dev"
-          element={<Coding_mountain_react_dev_home_page />}
-        >
+        {/*<Route path="/" element={<HomePage />} />*/}
+        <Route path="/" element={<Coding_mountain_react_dev_home_page />}>
           <Route index element={<ProductList />} />
-          <Route
-            path="coding-mountain-react-dev/products/:id"
-            element={<ProductDetail />}
-          />
+          <Route path="/products/:id" element={<ProductDetail />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
