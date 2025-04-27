29d8fad71942e096c38d859e903b9538dbd71fca
rohit <rohiitrb@gmail.com>
2025-04-27 11:02:18 +0530

    added error handling

--- web/src/routes/+page.server.ts
+++ web/src/routes/+page.server.ts
@@ -1,16 +1,21 @@
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-	const fetchWakatimeData = async () => {
-		const api = import.meta.env.VITE_API;
+  const fetchWakatimeData = async () => {
+    const api = import.meta.env.VITE_API;
 
-		const res = await fetch(`${api}/wakatime`);
-		if (!res.ok) {
-			throw new Error("Failed to fetch data");
+		try{
+			    const res = await fetch(`${api}/wakatime`);
+    if (!res.ok) {
+      return "Failed to fetch data";
+    }
+    return await res.json();
+
+		}catch{
+      return "Failed to fetch data";
 		}
-		return await res.json();
-	};
-	return {
-		wakatimeData: await fetchWakatimeData(),
-	};
+  };
+  return {
+    wakatimeData: await fetchWakatimeData(),
+  };
 };
