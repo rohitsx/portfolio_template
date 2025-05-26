fd696c7e3c31eaeef980db7bfa981a040b64d8d4
rohit <rohiitrb@gmail.com>
2025-05-26 21:35:52 +0530

    added socials

--- backup/web/src/components/bio.svelte
+++ backup/web/src/components/bio.svelte
@@ -1,5 +1,5 @@
 <div class="space-y-4">
-	<div class="flex justify-center space-x-4 mb-4 text-sm md:text-lg">
+	<div class="flex px-4 md:px-0 space-x-4 mb-4 text-sm md:text-lg">
 		<a
 			href="https://www.linkedin.com/in/rohitsx"
 			target="_blank"
