ac802d7607e98d3a359b994ae53b312ae2e7ebfd
rohit <rohiitrb@gmail.com>
2025-05-27 06:35:53 +0530

    updated boder

--- backup/web/src/routes/+layout.svelte
+++ backup/web/src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 	import "../app.css";
 </script>
 
-<div class="px-0 md:px-54 mx-auto text-lg">
+<div class="px-0 md:px-50 mx-auto text-lg">
 	{@render children()}
 </div>
--- backup/web/src/routes/+page.svelte
+++ backup/web/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <div class="md:flex gap-8 py-8 md:flex-row">
 	<div class="pr-8 md:pr-0 md:w-2/5"><Bio /></div>
 	<div class="md:w-3/5 space-y-12 px-3 md:px-0">
-		<div class="sm:flex md:flex-row gap-8">
+		<div class="space-y-12 md:space-y-0 md:flex md:flex-row md:gap-8">
 			<Saas />
 			<ChromeExtension />
 		</div>
